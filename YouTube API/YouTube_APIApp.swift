c389c11b0cd5ec2bb6af77658b03f5c866fa9b48
klpindoria <44475172+klpindoria@users.noreply.github.com>
2021-10-16 22:37:35 +0100

    Initial Commit

---
+++ YouTube API/YouTube_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTube_APIApp.swift
+//  YouTube API
+//
+//  Created by Ketan Pindoria on 16/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct YouTube_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
